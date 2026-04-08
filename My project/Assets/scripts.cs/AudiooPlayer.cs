5a5fec5858552ed202d3fce162ca8d1088cde124
Delmiro da Silva Oliveira <delmiro.s@escolar.ifrn.edu.br>
2026-04-08 17:03:56 -0300

    ad

--- My project/Assets/scripts.cs/AudiooPlayer.cs
+++ My project/Assets/scripts.cs/AudiooPlayer.cs
@@ -1,11 +1,35 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
-public class AudioPlayer : MonoBehaviour
+namespace scripts.cs
 {
-    public AudioCollection myAudioCollection;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class AudioPlayer : MonoBehaviour
     {
-        AudioManager.Instance.PlaySound(myAudioCollection.AudioClipCollection[0]);
+        public AudioCollection myAudioCollection;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+            if (myAudioCollection != null && myAudioCollection.AudioClipCollection.Count > 0)
+            {
+                AudioManager.Instance.PlaySound(myAudioCollection.AudioClipCollection[0]);
+            }
+            // Load the open scene additively
+            string sceneToLoad = PlayerPrefs.GetString("CurrentScene", "SampleScene");
+            if (sceneToLoad == "boot")
+            {
+                sceneToLoad = "SampleScene";
+            }
+            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Additive);
+            // Unload the boot scene
+            StartCoroutine(UnloadBoot());
+        }
+
+        private IEnumerator UnloadBoot()
+        {
+            yield return new WaitForSeconds(0.1f); // Wait a bit for the scene to load
+            SceneManager.UnloadSceneAsync("boot");
+        }
     }
 }
